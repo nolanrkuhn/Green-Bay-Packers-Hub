9b172cf344eb326006c111bcc0ae954e7a8a84ab
nolanrkuhn <nolanrkuhn@gmail.com>
2025-01-06 18:43:00 -0700

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     
     # Use the DATABASE_URL environment variable provided by Render
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://green_bay_packer_database_user:2Wui3XrzGZnpFmSxUqql1yBi1oNKiWyY@dpg-cruf69tumphs73emb5ag-a.oregon-postgres.render.com/green_bay_packer_database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://packers_sql_user:RL7fYgEnKAlAsATvZDhnUp0pLA6ggJQj@dpg-ctrbj5ogph6c73cvi12g-a.oregon-postgres.render.com/packers_sql'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
